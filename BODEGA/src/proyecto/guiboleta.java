4fc12e3b807741ad128aac6627f0820a815c60dd
Erick17-30 <erick.ooo822@gmail.com>
2025-04-26 11:37:35 -0500

    Commi

--- BODEGA/src/proyecto/guiboleta.java
+++ BODEGA/src/proyecto/guiboleta.java
@@ -30,27 +30,12 @@ public class guiboleta extends JDialog {
 	 * Create the dialog.
 	 */
 	public guiboleta() {
-		setBounds(100, 100, 450, 300);
+		setTitle("BOLETA DE VENTA");
+		setBounds(100, 100, 640, 635);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		contentPanel.setLayout(null);
 	}
 
 }
--- BODEGA/src/proyecto/interfaz.java
+++ BODEGA/src/proyecto/interfaz.java
@@ -14,21 +14,38 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
 
 public class interfaz extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtcode;
-	private JTextField txtcantidad;
-	private JTextField txtproduc;
-	private JTextField txtdni;
-	private JTextField txtstockact;
-	private JTextField txtstockrest;
-	private JTextField txtprecio;
-	private JTextField txttotal;
-	private JTextField txtprove;
+	private JTabbedPane tabbedPane;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JTextField textField;
+	private JLabel lblNewLabel_1_1;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_2_1;
+	private JTextField textField_3;
+	private JLabel lblNewLabel_2_1_1;
+	private JLabel lblNewLabel_2_1_1_1;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JLabel lblNewLabel_2_1_1_2;
+	private JLabel lblNewLabel_2_1_1_1_1;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JLabel lblNewLabel_2_2;
+	private JTextField textField_8;
+	private JScrollPane scrollPane;
 	private JButton btnIngresarDatos;
+	private JButton btnGenerarBolelta;
 	private JButton btnRegistrar;
 
 	/**
@@ -52,137 +69,160 @@ public class interfaz extends JFrame implements ActionListener {
 	 */
 	public interfaz() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1145, 489);
+		setBounds(100, 100, 1168, 573);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("BODEGA TECPROO");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
-		lblNewLabel.setBounds(469, 9, 165, 45);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Código de producto:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_1.setBounds(20, 65, 165, 24);
-		contentPane.add(lblNewLabel_1);
-		
-		txtcode = new JTextField();
-		txtcode.setBounds(151, 69, 134, 19);
-		contentPane.add(txtcode);
-		txtcode.setColumns(10);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Cantidad a adquirir:");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_1_1.setBounds(369, 64, 165, 24);
-		contentPane.add(lblNewLabel_1_1);
-		
-		txtcantidad = new JTextField();
-		txtcantidad.setColumns(10);
-		txtcantidad.setBounds(502, 69, 112, 19);
-		contentPane.add(txtcantidad);
-		
-		JLabel lblNewLabel_2 = new JLabel("Producto");
-		lblNewLabel_2.setBounds(655, 128, 73, 24);
-		contentPane.add(lblNewLabel_2);
-		
-		txtproduc = new JTextField();
-		txtproduc.setBounds(738, 131, 156, 19);
-		contentPane.add(txtproduc);
-		txtproduc.setColumns(10);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Ingresar DNI:");
-		lblNewLabel_2_1.setBounds(924, 128, 91, 24);
-		contentPane.add(lblNewLabel_2_1);
-		
-		txtdni = new JTextField();
-		txtdni.setBounds(1025, 131, 96, 19);
-		contentPane.add(txtdni);
-		txtdni.setColumns(10);
-		
-		JLabel lblNewLabel_2_1_1 = new JLabel("Stock actual:");
-		lblNewLabel_2_1_1.setBounds(655, 175, 91, 24);
-		contentPane.add(lblNewLabel_2_1_1);
-		
-		JLabel lblNewLabel_2_1_1_1 = new JLabel("Stock restante:");
-		lblNewLabel_2_1_1_1.setBounds(924, 175, 91, 24);
-		contentPane.add(lblNewLabel_2_1_1_1);
-		
-		txtstockact = new JTextField();
-		txtstockact.setColumns(10);
-		txtstockact.setBounds(739, 178, 96, 19);
-		contentPane.add(txtstockact);
-		
-		txtstockrest = new JTextField();
-		txtstockrest.setColumns(10);
-		txtstockrest.setBounds(1025, 178, 96, 19);
-		contentPane.add(txtstockrest);
-		
-		JLabel lblNewLabel_2_1_1_2 = new JLabel("Precio unitario:");
-		lblNewLabel_2_1_1_2.setBounds(655, 236, 134, 24);
-		contentPane.add(lblNewLabel_2_1_1_2);
-		
-		JLabel lblNewLabel_2_1_1_1_1 = new JLabel("Precio total:");
-		lblNewLabel_2_1_1_1_1.setBounds(924, 236, 91, 24);
-		contentPane.add(lblNewLabel_2_1_1_1_1);
-		
-		txtprecio = new JTextField();
-		txtprecio.setColumns(10);
-		txtprecio.setBounds(739, 239, 96, 19);
-		contentPane.add(txtprecio);
-		
-		txttotal = new JTextField();
-		txttotal.setColumns(10);
-		txttotal.setBounds(1025, 239, 96, 19);
-		contentPane.add(txttotal);
-		
-		JLabel lblNewLabel_2_2 = new JLabel("Proveedor:");
-		lblNewLabel_2_2.setBounds(655, 290, 73, 24);
-		contentPane.add(lblNewLabel_2_2);
-		
-		txtprove = new JTextField();
-		txtprove.setColumns(10);
-		txtprove.setBounds(738, 293, 156, 19);
-		contentPane.add(txtprove);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(20, 105, 584, 315);
-		contentPane.add(scrollPane);
-		
-		JTextArea textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
-		
-		btnIngresarDatos = new JButton("Ingresar datos");
-		btnIngresarDatos.addActionListener(this);
-		btnIngresarDatos.setBounds(981, 281, 140, 33);
-		contentPane.add(btnIngresarDatos);
-		
-		JButton btnGenerarBolelta = new JButton("Generar boleta");
-		btnGenerarBolelta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		{
+			tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+			tabbedPane.setBounds(10, 10, 1134, 516);
+			contentPane.add(tabbedPane);
+			{
+				panel = new JPanel();
+				tabbedPane.addTab("Ventas", null, panel, null);
+				panel.setLayout(null);
+				{
+					lblNewLabel = new JLabel("BODEGA TECPROO");
+					lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+					lblNewLabel.setBounds(459, 23, 165, 45);
+					panel.add(lblNewLabel);
+				}
+				{
+					lblNewLabel_1 = new JLabel("Código de producto:");
+					lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+					lblNewLabel_1.setBounds(10, 79, 165, 24);
+					panel.add(lblNewLabel_1);
+				}
+				{
+					textField = new JTextField();
+					textField.setColumns(10);
+					textField.setBounds(141, 83, 134, 19);
+					panel.add(textField);
+				}
+				{
+					lblNewLabel_1_1 = new JLabel("Cantidad a adquirir:");
+					lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+					lblNewLabel_1_1.setBounds(359, 78, 165, 24);
+					panel.add(lblNewLabel_1_1);
+				}
+				{
+					textField_1 = new JTextField();
+					textField_1.setColumns(10);
+					textField_1.setBounds(492, 83, 112, 19);
+					panel.add(textField_1);
+				}
+				{
+					lblNewLabel_2 = new JLabel("Producto");
+					lblNewLabel_2.setBounds(645, 142, 73, 24);
+					panel.add(lblNewLabel_2);
+				}
+				{
+					textField_2 = new JTextField();
+					textField_2.setEditable(false);
+					textField_2.setColumns(10);
+					textField_2.setBounds(728, 145, 156, 19);
+					panel.add(textField_2);
+				}
+				{
+					lblNewLabel_2_1 = new JLabel("Ingresar DNI:");
+					lblNewLabel_2_1.setBounds(914, 142, 91, 24);
+					panel.add(lblNewLabel_2_1);
+				}
+				{
+					textField_3 = new JTextField();
+					textField_3.setColumns(10);
+					textField_3.setBounds(1015, 145, 96, 19);
+					panel.add(textField_3);
+				}
+				{
+					lblNewLabel_2_1_1 = new JLabel("Stock actual:");
+					lblNewLabel_2_1_1.setBounds(645, 189, 91, 24);
+					panel.add(lblNewLabel_2_1_1);
+				}
+				{
+					lblNewLabel_2_1_1_1 = new JLabel("Stock restante:");
+					lblNewLabel_2_1_1_1.setBounds(914, 189, 91, 24);
+					panel.add(lblNewLabel_2_1_1_1);
+				}
+				{
+					textField_4 = new JTextField();
+					textField_4.setColumns(10);
+					textField_4.setBounds(729, 192, 96, 19);
+					panel.add(textField_4);
+				}
+				{
+					textField_5 = new JTextField();
+					textField_5.setColumns(10);
+					textField_5.setBounds(1015, 192, 96, 19);
+					panel.add(textField_5);
+				}
+				{
+					lblNewLabel_2_1_1_2 = new JLabel("Precio unitario:");
+					lblNewLabel_2_1_1_2.setBounds(645, 250, 134, 24);
+					panel.add(lblNewLabel_2_1_1_2);
+				}
+				{
+					lblNewLabel_2_1_1_1_1 = new JLabel("Precio total:");
+					lblNewLabel_2_1_1_1_1.setBounds(914, 250, 91, 24);
+					panel.add(lblNewLabel_2_1_1_1_1);
+				}
+				{
+					textField_6 = new JTextField();
+					textField_6.setColumns(10);
+					textField_6.setBounds(729, 253, 96, 19);
+					panel.add(textField_6);
+				}
+				{
+					textField_7 = new JTextField();
+					textField_7.setColumns(10);
+					textField_7.setBounds(1015, 253, 96, 19);
+					panel.add(textField_7);
+				}
+				{
+					lblNewLabel_2_2 = new JLabel("Proveedor:");
+					lblNewLabel_2_2.setBounds(645, 304, 73, 24);
+					panel.add(lblNewLabel_2_2);
+				}
+				{
+					textField_8 = new JTextField();
+					textField_8.setColumns(10);
+					textField_8.setBounds(728, 307, 156, 19);
+					panel.add(textField_8);
+				}
+				{
+					scrollPane = new JScrollPane();
+					scrollPane.setBounds(10, 119, 584, 315);
+					panel.add(scrollPane);
+				}
+				{
+					btnIngresarDatos = new JButton("Ingresar datos");
+					btnIngresarDatos.setBounds(971, 295, 140, 33);
+					panel.add(btnIngresarDatos);
+				}
+				{
+					btnGenerarBolelta = new JButton("Generar boleta");
+					btnGenerarBolelta.setBounds(645, 401, 140, 33);
+					panel.add(btnGenerarBolelta);
+				}
+				{
+					btnRegistrar = new JButton("Registrar");
+					btnRegistrar.setBounds(728, 76, 140, 33);
+					panel.add(btnRegistrar);
+				}
 			}
-		});
-		btnGenerarBolelta.setBounds(655, 387, 140, 33);
-		contentPane.add(btnGenerarBolelta);
-		
-		btnRegistrar = new JButton("Registrar");
-		btnRegistrar.addActionListener(this);
-		btnRegistrar.setBounds(738, 62, 140, 33);
-		contentPane.add(btnRegistrar);
+			{
+				panel_1 = new JPanel();
+				tabbedPane.addTab("Consulta ", null, panel_1, null);
+			}
+			{
+				panel_2 = new JPanel();
+				tabbedPane.addTab("Proveedores", null, panel_2, null);
+				panel_2.setLayout(null);
+			}
+		}
 	}
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnRegistrar) {
-			do_btnRegistrar_actionPerformed(e);
-		}
-		if (e.getSource() == btnIngresarDatos) {
-			do_btnIngresarDatos_actionPerformed(e);
-		}
-	}
-	protected void do_btnIngresarDatos_actionPerformed(ActionEvent e) {
-	}
-	protected void do_btnRegistrar_actionPerformed(ActionEvent e) {
-        getToolkit().getDefaultToolkit().beep();
 	}
 }
